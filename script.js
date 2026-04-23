ba3cf23f5ccb40dd09b00234c06dbaedac714273
Gitmaster5000-glitch <bluelord55555@gmail.com>
2026-04-23 18:07:12 +0300

    Implement counter animation on scroll event
    Add counter animation that increments numbers on scroll

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ section.classList.add('show');
 }
 });
 });
+// Counter Animation
+const counters = document.querySelectorAll('.counter');
+
+const runCounter = () => {
+counters.forEach(counter => {
+const target = +counter.getAttribute('data-target');
+const increment = target / 200;
+
+const updateCount = () => {
+const count = +counter.innerText;
+
+if(count < target){
+counter.innerText = Math.ceil(count + increment);
+setTimeout(updateCount, 10);
+} else {
+counter.innerText = target;
+}
+};
+
+updateCount();
+});
+};
+
+window.addEventListener("scroll", () => {
+const statsSection = document.querySelector(".stats");
+if(statsSection.getBoundingClientRect().top < window.innerHeight){
+runCounter();
+}
+});
