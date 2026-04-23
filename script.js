59fcee36e99a74f0abc9618c2147b945defbaa51
Gitmaster5000-glitch <bluelord55555@gmail.com>
2026-04-23 18:51:30 +0300

    Implement counter animation and update comments
    Added counter animation functionality and updated comments.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Scroll animation
+// SCROLL ANIMATION
 const faders = document.querySelectorAll('.fade');
 
 const appearOnScroll = new IntersectionObserver(
@@ -16,7 +16,28 @@ faders.forEach(fader=>{
 appearOnScroll.observe(fader);
 });
 
-// Modal functions
+// COUNTER ANIMATION
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter=>{
+const updateCount = ()=>{
+const target = +counter.getAttribute('data-target');
+const count = +counter.innerText;
+
+const increment = target / 200;
+
+if(count < target){
+counter.innerText = Math.ceil(count + increment);
+setTimeout(updateCount,10);
+}else{
+counter.innerText = target;
+}
+};
+
+updateCount();
+});
+
+// MODAL
 function openModal(image,name){
 document.getElementById('modal').style.display="flex";
 document.getElementById('modal-img').src=image;
